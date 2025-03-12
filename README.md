69c905cb08fc419a78546a8b1d0e58cb44691c32
querydavid <davidjp250@gmail.com>
2025-03-12 22:54:37 +0700

    add histogram and fix misc things

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # clipgraph
-a simple CLI to rapidly parse and graph data from the system clipboard
+A simple CLI to parse and graph numerical data pasted to the clipboard
+
+Uses the [rasciigraph](https://github.com/orhanbalci/rasciigraph) and [histo](https://docs.rs/histo/latest/histo/index.html) crates.
+
+Usage:
+
+`
+clipgraph [OPTIONS]
+`
+
+Input data from the system clipboard should be newline-delimited. Not robust yet but works with e.g. copying a column from a spreadsheet.
+
+Case-sensitive options:
+- "p" Prints the numerical data that could be parsed from the clipboard
+- "l" ASCII line graph of the values (in the order presented)
+- "h" 10-bucket histogram with exploratory stats (currently supports only positive numbers, and rounds to integers)
+
+To-dos:
+- Work with other delimiters
+- add Clap, the CLI is currently very minimal
+- error handling
+- throw in some other plotting libraries
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,55 @@
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
+use histo::Histogram;
 use rasciigraph::{plot, Config};
+use std::num::ParseFloatError;
 
-fn main() {
-    let sraw = "230
-    230
-    228
-    228
-    227
-    226
-    228
-    229
-    227
-    225
-    223
-    230
-    231
-    233
-    223
-    212
-    204
-    199
-    194
-    191
-    190
-    188
-    195
-    197.9
-    197.8
-    197.6
-    197.5";
-
-    let mut traw: ClipboardContext = ClipboardProvider::new().unwrap();
-    let raw = traw.get_contents().unwrap();
-
+fn main() -> Result<(), ParseFloatError> {
+    let mut craw: ClipboardContext = ClipboardProvider::new().unwrap();
+    let raw = craw.get_contents().unwrap();
+    craw.set_contents(raw.clone()).unwrap(); //this is to "reset" clipboard contents
+                                             //I wonder if there is a better way to do this without clone
+                                             //add error handling and not sure if that actually works.
     let weight: Vec<&str> = raw.split("\n").collect();
     //println!("{:?}", weight);
     let mut weights: Vec<f64> = Vec::new();
     for w in weight {
-        let x: f64 = w.trim().parse().unwrap(); //add error handling
+        let x: f64 = w.trim().parse()?; //add better error handling
         weights.push(x);
     }
-    //println!("{:?}", weights);
+
+    let mut mode = std::env::args();
+    match mode.nth(1).unwrap().as_str() {
+        //I sense this is a very messy unwrap
+        "p" => printdata(weights),
+        "l" => linedata(weights),
+        "h" => histodata(weights),
+        _ => println!("Usage: 'p' to print data, 'l' for line, 'h' for histogram"),
+    };
+
+    Ok(())
+}
+
+fn printdata(weights: Vec<f64>) {
+    println!("{:?}", weights);
+}
+
+fn linedata(weights: Vec<f64>) {
     println!(
         "{}",
-        plot(weights, Config::default()) //.with_height(10).with_offset(10))
+        plot(weights, Config::default().with_height(10).with_offset(10))
     );
+}
+
+fn histodata(weights: Vec<f64>) {
+    let mut histogram = Histogram::with_buckets(10);
+    let mut rounded: Vec<u64> = Vec::new();
+    for x in weights {
+        if x < 0.0 {
+            panic!("histogram currently doesn't support negatives")
+        }
+        let y = x as u64;
+        histogram.add(y); //this is obviously bad and will fail
+    }
+    println!("{}", histogram);
 }
